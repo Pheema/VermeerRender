baf61200050c40dc786b6363644a0fc19cc1c89b
Pheema <y.itou.physics@gmail.com>
2016-08-08 03:05:36 +0900

    Add Vector3f.[hpp/cpp]

---
+++ VermeerRender/Vector3f.hpp
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <iostream>
+
+namespace VermeerRender
+{
+	class Vector3f
+	{
+	public:
+		float x, y, z;
+		Vector3f() {}
+		explicit Vector3f(float f) { this->x = f; this->y = f; this->z = f; }
+		Vector3f(float x, float y, float z) { this->x = x; this->y = y; this->z = z; }
+
+		// Vector3f + Vector3f
+		inline Vector3f
+		operator+(Vector3f& v) const { return Vector3f(x + v.x, y + v.y, z + v.z); }
+		
+		// Vector3f - Vector3f
+		inline Vector3f
+		operator-(Vector3f& v) const { return Vector3f(x - v.x, y - v.y, z - v.z); }
+
+		// Vector3f * Vector3f (óvëfñà)
+		inline Vector3f
+		operator*(Vector3f& v) const { return Vector3f(x * v.x, y * v.y, z * v.z); }
+
+		// Vector3f / Vector3f (óvëfñà)
+		inline Vector3f
+		operator/(Vector3f& v) const { return Vector3f(x / v.x, y / v.y, z / v.z); }
+
+		// Vector3f * number
+		template <typename T>
+		inline Vector3f
+		operator*(T a) const { return Vector3f(x * a, y * a, z * a); }
+
+		// number * Vector3f
+		template<typename T>
+		friend inline
+		Vector3f operator*(T a, Vector3f& v);
+
+	private:
+		
+	};
+
+	// number * Vector3f
+	template<typename T>
+	Vector3f
+	operator*(T a, Vector3f& v) { return v * a };
+
+	// ì‡êœ
+	float
+	Dot(const Vector3f& v1, const Vector3f& v2) { return v1.x * v2.x + v1.y * v2.y + v1.z * v2.z; }
+
+	// äOêœ
+	Vector3f
+	Cross(const Vector3f& v1, const Vector3f& v2)
+	{
+		Vector3f v;
+		v.x = v1.y * v2.z - v1.z * v2.y;
+		v.y = v1.z * v2.x - v1.x * v2.z;
+		v.z = v1.x * v2.y - v1.y * v2.x;
+		return v;
+	}
+
+	std::ostream&
+	operator<<(std::ostream& os, const Vector3f& v)
+	{
+		os << "(" << v.x << ", " << v.y << ", " << v.z << ")";
+		return os;
+	}
+}
--- VermeerRender/VermeerRender.cpp
+++ VermeerRender/VermeerRender.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include "Vector3f.hpp"
+using namespace VermeerRender;
 
 int main()
 {
 	std::cout << "This is the raytracer for RayTracing Camp 4!?." << std::endl;
+	Vector3f v1(1, 0, 0);
+	Vector3f v2(0, 1, 0);
+
+	std::cout << Cross(v1, v2) << std::endl;
+
 	return 0;
 }
