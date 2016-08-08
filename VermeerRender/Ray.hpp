1c350f8a34f028452ca78ae6c1e39154eb2df24b
Pheema <y.itou.physics@gmail.com>
2016-08-08 20:30:57 +0900

    Ray.hppを追加、Tabを空白に、文字コードをUTF8に

---
+++ VermeerRender/Ray.hpp
@@ -0,0 +1,29 @@
+﻿#pragma once
+
+#include "Vector3f.hpp"
+
+namespace VermeerRender
+{
+    enum class RayTypes
+    {
+        CAMERA,
+        SHADOW,
+        DIFFUSE,
+        TRANSLUCENT,
+        GLOSSY
+    };
+
+    class Ray
+    {
+    public:
+        Vector3f o;				// Ray origin
+        Vector3f dir;			// Ray direction
+
+        float ior;				// レイが飛んでいるマテリアルの屈折率
+        unsigned int bounce;	// レイが反射した回数
+
+        Ray() {};
+        Ray(const Vector3f& o, const Vector3f& d, float ior = 1.0f, unsigned int bounce = 0)
+            : o(o), dir(dir), ior(ior), bounce(bounce) {};
+    };
+}
--- VermeerRender/VermeerRender.cpp
+++ VermeerRender/VermeerRender.cpp
@@ -1,14 +1,14 @@
-#include <iostream>
+﻿#include <iostream>
 #include "Vector3f.hpp"
 using namespace VermeerRender;
 
 int main()
 {
-	std::cout << "This is the raytracer for RayTracing Camp 4!?." << std::endl;
-	Vector3f v1(1, 0, 0);
-	Vector3f v2(0, 1, 0);
+    std::cout << "This is the raytracer for RayTracing Camp 4!?." << std::endl;
+    Vector3f v1(1, 0, 0);
+    Vector3f v2(0, 1, 0);
 
-	std::cout << Cross(v1, v2) << std::endl;
+    std::cout << Cross(v1, v2) << std::endl;
 
-	return 0;
+    return 0;
 }
